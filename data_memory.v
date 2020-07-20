8f91b15a4b2cfe65ff7e53bfc6db3bb072a3dc50
pr1266 <pourya_1999@yahoo.com>
2020-07-20 16:21:54 -0700

    'mem_v0'

--- data_memory.v
+++ data_memory.v
@@ -8,4 +8,28 @@ module data_memory(input clk,
 
 reg [31:0] mem [127:0];
 
+integer i;
+
+initial begin
+	// be soorat e pishfarz miaim tamam e cell haye hafeze ro ba 0 por mikonim
+	read_data <= 0;
+	for (i = 0; i < 128; i = i + 1) 
+	begin
+		mem[i] = 0;
+	end
+	// az ye file bekhoonim :
+	//$readmemh("memory.list", mem);
+end
+
+always @(posedge clk)
+begin
+
+	// age memRead on bood :
+	if(memRead) read_data <= mem[addr];
+	
+	// age memWrite on bood :
+	if(memWrite) mem[addr] <= write_data;
+
+end
+
 endmodule
