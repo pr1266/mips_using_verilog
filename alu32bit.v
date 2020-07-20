040243b2493ad32683a94f245707b6b5cc8dec35
pr1266 <pourya_1999@yahoo.com>
2020-07-20 15:44:22 -0700

    'alu_v1'

--- alu32bit.v
+++ alu32bit.v
@@ -13,7 +13,14 @@ begin
 	4'b0000 : out = first && second;
 	// or operation
 	4'b0001 : out = first || second;
-	
+	// add operation
+	4'b0010 : out = first + second;
+	// subtract operation
+	4'b0110 : out = first - second;
+	// set on less than (SLT) : 
+	/**/
+	// nor
+	/**/
 	endcase
 end
 endmodule
